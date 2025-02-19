7ee602c1e4cc226b0f78a15e776f242aba4bbecb
monouj <diego32442@ephemeraleden.cloud>
2025-02-19 12:40:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import re
 import aiohttp  # Using aiohttp for async HTTP requests
 from web import keep_alive
 
-BOT_TOKEN = "6880096605:AAEBk0D_B-c1lTEj-kfva-UXkHOds8d-jxY"
+BOT_TOKEN = "7334566762:AAHmhSrMqjAR-9gJOlXzatb0TVorstDQBwk"
 BASE_URL = "https://opabhik.serv00.net/Watch.php?url="
 TERABOX_PATTERN = r"https?://(?:\w+\.)?(terabox|1024terabox|freeterabox|teraboxapp|tera|teraboxlink|mirrorbox|nephobox|1024tera|momerybox|tibibox|terasharelink|teraboxshare|terafileshare)\.\w+"
 LOG_CHANNEL_ID = "-1002107937108"  # Replace with your actual log channel's username or chat ID
