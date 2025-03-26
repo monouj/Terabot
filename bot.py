70ad8e449bf9f3fdbc6606e65871baf1b88abb2a
monouj <diego32442@ephemeraleden.cloud>
2025-03-26 07:48:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import re
 import aiohttp  # Using aiohttp for async HTTP requests  
 from web import keep_alive  
 
-BOT_TOKEN = "6880096605:AAEB9rVeTYttdDqH6BM2nn4rml_m_DqiqaA"  
+BOT_TOKEN = "6880096605:AAHBDrFX1ZdSnNR1OZ0nXdXdZDppttBtngI"  
 BASE_URL = "https://opabhik.serv00.net/Watch.php?url="  
 DOWNLOAD_BASE = "https://teradlrobot.cheemsbackup.workers.dev/?url="  
 TERABOX_PATTERN = r"https?://(?:\w+\.)?(terabox|1024terabox|freeterabox|teraboxapp|tera|teraboxlink|mirrorbox|nephobox|1024tera|momerybox|tibibox|terasharelink|teraboxshare|terafileshare)\.\w+"  
